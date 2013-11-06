face490a02b0e7cce282e7eedfaf6a4facb44a13
姜勇 <steven@kyouisamutekiMacBook-Air.local>
2013-11-06 13:41:47 +0800

    Initial Commit

---
+++ QingDaoNews/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QingDaoNews
+//
+//  Created by 姜勇 on 13-11-6.
+//  Copyright (c) 2013年 姜勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
